3ea8717957f64ab05d516f1f637ae1ff92962e7a
Michelle Alicia <winkydeep@gmail.com>
2026-04-24 13:21:37 +0700

    udh bisa login & pidnah dashboard

--- app/src/main/java/com/example/habittracker/view/LoginFragment.kt
+++ app/src/main/java/com/example/habittracker/view/LoginFragment.kt
@@ -5,13 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.navigation.Navigation
 import com.example.habittracker.R
 
 class LoginFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val username = view.findViewById<EditText>(R.id.edtUsername)
+        val password = view.findViewById<EditText>(R.id.edtPassword)
+        val btnLogin = view.findViewById<Button>(R.id.btnLogin)
+
+        btnLogin.setOnClickListener {
+            val user = username.text.toString()
+            val pass = password.text.toString()
+
+            Toast.makeText(requireContext(), "$user - $pass", Toast.LENGTH_SHORT).show()
+
+            if (user == "student" && pass == "123") {
+                Navigation.findNavController(view)
+                    .navigate(R.id.action_loginFragment_to_dashboardFragment)
+            } else {
+                Toast.makeText(requireContext(), "Login gagal", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
